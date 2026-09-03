1287ade1346f83c2e9ba0467d6f5d1cee7a1af9b
ykteo000 <177651145+ykteo000@users.noreply.github.com>
2026-09-03 20:21:49 +0800

    Add JavaFX Tutorial-Part1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // UI text element
+        Scene scene = new Scene(helloWorld);          // Canvas containing the label
+        stage.setScene(scene);                        // Put canvas in window
+        stage.show();                                 // Display window
+    }
 }
